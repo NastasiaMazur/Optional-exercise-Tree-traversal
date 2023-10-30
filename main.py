57f62e3bb6037103890fc15cb90e8017b4f3862e
NastasiaMazur <nvmazour@mail.ru>
2023-10-30 15:10:34 +0100

    Part 2

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 # Optional exercise: Tree traversal #
 
 # Part 1
-
+print('PART 1')
 class TreeNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-    def traverse_in_order(self, visitor):
-        if self.left:
-            self.left.traverse_in_order(visitor)
-        visitor(self)
-        if self.right:
-            self.right.traverse_in_order(visitor)
-
-    def traverse_pre_order(self, visitor):
-        visitor(self)
-        if self.left:
-            self.left.traverse_pre_order(visitor)
-        if self.right:
-            self.right.traverse_pre_order(visitor)
-
-    def traverse_post_order(self, visitor):
-        if self.left:
-            self.left.traverse_post_order(visitor)
-        if self.right:
-            self.right.traverse_post_order(visitor)
-        visitor(self)
-
 def visit(node):
-    print(f"Visiting {node.value}")
+    print(node.value, end=', ')
+
+def traverse_recursively_in_order(node):
+    if node is not None:
+        traverse_recursively_in_order(node.left)
+        visit(node)
+        traverse_recursively_in_order(node.right)
+
+def traverse_in_order_imperatively(node):
+    stack = []
+    current = node
+
+    while stack or current:
+        if current:
+            stack.append(current)
+            current = current.left
+        else:
+            current = stack.pop()
+            visit(current)
+            current = current.right
+
+
+# Construct the tree
+
 
 #My tree:
 root = TreeNode("+")
@@ -44,14 +46,43 @@ root.right = TreeNode("+")
 root.right.left = TreeNode("D")
 root.right.right = TreeNode("E")
 
+
 print("Recursive In-Order Traversal:")
-root.traverse_in_order(visit)
+traverse_recursively_in_order(root)
 
-
-print("\nRecursive Pre-Order Traversal:")
-root.traverse_pre_order(visit)
-
-print("\nRecursive Post-Order Traversal:")
-root.traverse_post_order(visit)
+print("\nImperative In-Order Traversal:")
+traverse_in_order_imperatively(root)
 
 # Part 2 (Recursive only)
+
+print('\n\n''PART 2')
+
+def traverse_in_order(node, visitor):
+    if node is not None:
+        traverse_in_order(node.left, visitor)
+        visitor(node)
+        traverse_in_order(node.right, visitor)
+
+
+def traverse_pre_order(node, visitor):
+    if node is not None:
+        visitor(node)
+        traverse_pre_order(node.left, visitor)
+        traverse_pre_order(node.right, visitor)
+
+
+def traverse_post_order(node, visitor):
+    if node is not None:
+        traverse_post_order(node.left, visitor)
+        traverse_post_order(node.right, visitor)
+        visitor(node)
+
+
+print("In-order: ", end='')
+traverse_in_order(root, visit)
+
+print("\nPre-order: ", end='')
+traverse_pre_order(root, visit)
+
+print("\nPost-order: ", end='')
+traverse_post_order(root, visit)
